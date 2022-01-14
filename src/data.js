31a10068657e450028ec1157abe856473910100f
Chaitra <chaitra.srinivas@gmail.com>
2022-01-14 15:20:09 +1100

    Added projects and images

--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 
-export const projects = [
+const projects = [
   {
     title: "My FrontYard",
     subtitle: "Node.js, Express and MySQL",
     description:
       "My FrontYard is an intuitive app designed to allow hosts to advertise their residential events and allows other users to access event information and plan their journey to said events.",
-    image: "projectImg1",
+    image: "./images/MyFrontyard.png",
     link: "https://my-frontyard.herokuapp.com/",
     github: "https://github.com/chaitra-srinivas/myfrontyard",
   },
@@ -14,9 +14,46 @@ export const projects = [
     subtitle: "JavaScript and Third party APIs",
     description:
       "The Entertainment Hub is a web application that provides information about the current New York Times Best Sellers List, book and movie reviews and a list of critically acclaimed movies",
-    image: "./images/project-2.gif",
+    image: "./images/EntertainementHub.png",
     link: "https://chaitra-srinivas.github.io/The-Entertainment-Hub/",
     github: "https://github.com/chaitra-srinivas/The-Entertainment-Hub",
   },
-  
+  {
+    title: "Tech Talk",
+    subtitle: "JavaScript, Express, MySQL and Handlebars",
+    description:
+      "Tech Talk is a CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers’ posts as well. This application is built using JavaScript, Express.js, and Sequelize as the ORM to interact with MySQL. Handlebars.js is used as the templating engine. It follows the MVC paradigm in it's architectural structure.",
+    image: "./images/TechTalk.png",
+    link: "https://tech-talk-21.herokuapp.com/",
+    github: "https://github.com/chaitra-srinivas/Tech-Blog",
+  },
+  {
+    title: "Text Editor",
+    subtitle: "JavaScript, Express, IndexedDB and Webpack",
+    description:
+      "Text Editor is a single page web application that runs in the browser and meets the PWA criteria. The application is installable and can function offline. This application is built using JavaScript, Express.js. IndexedDB is used for data persistance on the client side.",
+    image: "./images/JATE.png",
+    link: "https://sleepy-beach-16739.herokuapp.com/",
+    github: "https://github.com/chaitra-srinivas/Text-Editor",
+  },
+  {
+    title: "README Generator",
+    subtitle: "JavaScript, Node.js and Inquirer package",
+    description:
+      "The README Generator can be used by developers to create a professional README.md file for their project. This is a CLI application built using node.js and the npm inquirer package.",
+    image: "./images/README.png",
+    link: "https://github.com/chaitra-srinivas/README-Generator",
+    github: "https://github.com/chaitra-srinivas/Text-Editor",
+  },
+  {
+    title: "Weather Dashboard",
+    subtitle: "JavaScript, HTML and CSS",
+    description:
+      "Using this web application a user can view the current weather and a five day forecast for a particular city",
+    image: "./images/WeatherDashboard.png",
+    link: "https://chaitra-srinivas.github.io/Weather-Dashboard/",
+    github: "https://github.com/chaitra-srinivas/Weather-Dashboard",
+  },
 ];
+
+export default projects;
